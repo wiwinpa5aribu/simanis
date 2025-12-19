76271672de3d71ae95674a4ca6cbccc21fa020c0
Wiwin Pasaribu <syirahamirahlubis@gmail.com>
2025-12-19 11:50:01 -0800

    chore: remove debug logs and restore ci cache

--- lib/services/location-service.ts
+++ lib/services/location-service.ts
@@ -10,8 +10,8 @@ export const locationService = {
      * @returns {TLocation[]} An array of validated location objects.
      */
     getAll: (): TLocation[] => {
-        console.log("SIMANIS_DEBUG: Executing locationService.getAll with updated nullable schema")
         return locations.map((loc) => {
+
 
             const result = locationSchema.safeParse(loc)
             if (!result.success) {
